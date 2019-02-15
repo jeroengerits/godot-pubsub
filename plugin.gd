c48663527a1e4957a67216a605d56c41942e556e
Jeroen Gerits <jeroengerits@fastmail.com>
2019-02-15 16:23:49 +0100

    Update plugin.gd

--- plugin.gd
+++ plugin.gd
@@ -2,7 +2,9 @@ tool
 extends EditorPlugin
 
 func _enter_tree():
-    add_custom_type("PubSub", "Node", preload("PubSub.gd"), preload("icon.png"))
+    # add_custom_type("PubSub", "Node", preload("PubSub.gd"), preload("icon.png"))
+    pass
 
 func _exit_tree():
-    remove_custom_type("PubSub")
+    # remove_custom_type("PubSub")
+    pass
